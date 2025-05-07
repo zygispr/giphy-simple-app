fcb21d2e4d140367987623df4d8bba35cd5a1946
zygispr <99207910+zygispr@users.noreply.github.com>
2025-05-07 17:13:51 +0300

    styling fixes

--- src/components/molecules/Card/Card.tsx
+++ src/components/molecules/Card/Card.tsx
@@ -13,14 +13,13 @@ export interface CardProps {
 
 function Card(props: CardProps) {
   const shortenedDate = props.date.slice(0, 10);
-  const shortenedLabel = props.label.split(/gif/i)[0].trim();
 
   return (
     <div className="card" id={props.id}>
       <img className="card__img" src={props.imgSrc} alt={props.label || "GIF"} onClick={() => props.onClick?.(props.id)} />
       {props.isLocked !== undefined && <div className="card__lock">{props.isLocked ? <LockIcon /> : <LockOpenIcon />}</div>}
       {props.date && <div className="card__date">{shortenedDate}</div>}
-      {props.label && <div className="card__label">{shortenedLabel}</div>}
+      {props.label && <div className="card__label">{props.label}</div>}
     </div>
   );
 }
