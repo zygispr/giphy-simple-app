b678601fc992b51899626aebbad9deb8d82bbeb1
zygispr <99207910+zygispr@users.noreply.github.com>
2025-05-07 19:13:32 +0300

    appState -> galleryState

--- src/store/selectors.ts
+++ src/store/selectors.ts
@@ -1,3 +1,3 @@
 import type { RootState } from "./store.ts";
 
-export const selectCards = (state: RootState) => state.app.cards;
+export const selectCards = (state: RootState) => state.gallery.cards;
